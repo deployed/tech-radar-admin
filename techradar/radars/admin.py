99bacdad2f2d2db96c59687317805702e7bcba36
Łukasz Chojnacki <lukasz.chojnacki@deployed.pl>
2025-01-14 15:32:57 +0100

    Add admin inline classes

--- techradar/radars/admin.py
+++ techradar/radars/admin.py
@@ -2,14 +2,26 @@ from django.contrib import admin
 from .models import Radar, Segment, Technology
 
 
+class SegmentInline(admin.TabularInline):
+    model = Segment
+    extra = 0
+
+
 @admin.register(Radar)
 class RadarAdmin(admin.ModelAdmin):
     list_display = ("label", "slug")
+    inlines = (SegmentInline,)
+
+
+class TechnologyInline(admin.StackedInline):
+    model = Technology
+    extra = 0
 
 
 @admin.register(Segment)
 class SegmentAdmin(admin.ModelAdmin):
     list_display = ("label", "slug")
+    inlines = (TechnologyInline,)
 
 
 @admin.register(Technology)
