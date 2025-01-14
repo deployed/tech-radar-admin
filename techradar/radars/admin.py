5ac70c799ed9fb7f29b091e81662045e69e26a7c
Justyna Gruba <justyna.gruba@deployed.pl>
2025-01-14 14:41:30 +0100

    Models added.

--- techradar/radars/admin.py
+++ techradar/radars/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import Radar
+from .models import Radar, Segment, Technology
 
 
 @admin.register(Radar)
 class RadarAdmin(admin.ModelAdmin):
     list_display = ("label", "slug")
+
+
+@admin.register(Segment)
+class SegmentAdmin(admin.ModelAdmin):
+    list_display = ("label", "slug")
+
+
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ("label", "slug", "link", "segment", "radar", "ring")
--- techradar/radars/models.py
+++ techradar/radars/models.py
@@ -10,6 +10,28 @@ class Radar(models.Model):
         return self.slug
 
 
-## TODO Segment model
+class Segment(models.Model):
+    label = models.CharField(max_length=20)
+    slug = AutoSlugField(populate_from="label")
 
-## TODO Technology model
+    def __str__(self):
+        return self.slug
+
+
+class Ring(models.TextChoices):
+    ADOPT = "adopt"
+    HOLD = "hold"
+    ASSESS = "assess"
+    TRIAL = "trial"
+
+
+class Technology(models.Model):
+    label = models.CharField(max_length=50)
+    slug = AutoSlugField(populate_from="label")
+    link = models.CharField(max_length=100)
+    segment = models.ForeignKey(Segment, on_delete=models.CASCADE, default=None)
+    radar = models.ForeignKey(Radar, on_delete=models.CASCADE, default=None)
+    ring = models.CharField(max_length=7, choices=Ring.choices, default=Ring.ADOPT)
+
+    def __str__(self):
+        return self.slug
