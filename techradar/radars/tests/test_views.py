404a9c765f1b01f214e7ccf0ea6409e5efec6082
Justyna Gruba <justyna.gruba@deployed.pl>
2025-01-22 16:10:49 +0100

    Code review improvements

--- techradar/radars/tests/test_views.py
+++ techradar/radars/tests/test_views.py
@@ -18,12 +18,5 @@ class TestRadarViewSet:
         RadarFactory(label="backend")
         response = api_client.get(reverse("radar-list"), {"slug": "frontend"})
 
-        assert response.status_code == 404
-        assert response.json() == {"detail": "Radar with this slug not found."}
-
-    def test_missing_slug(self, api_client):
-        RadarFactory(label="backend")
-        response = api_client.get(reverse("radar-list"))
-
-        assert response.status_code == 404
-        assert response.json() == {"detail": "Slug parameter is missing."}
+        assert response.status_code == 200
+        assert response.json() == []
